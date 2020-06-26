5022eacf9890537e7c08648a634e84dea727de83
Ali Helmy <alymohamad@gmail.com>
2020-06-26 21:58:18 +0200

    Fix docker file

--- core-server/src/main/docker/entrypoint.sh
+++ core-server/src/main/docker/entrypoint.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-spring_config_location=""
-if [[ -z "${CONFIGURATION_FILE}" ]]; then
-    spring_config_location=""
-else
-    spring_config_location="--spring.config.location=${CONFIGURATION_FILE}"
-fi
-
-echo "${spring_config_location}"
-java -jar /app.jar --spring.profiles.active=prod ${spring_config_location}
+java -jar /app.jar --spring.profiles.active=prod --spring.config.location=/config/application.yml
